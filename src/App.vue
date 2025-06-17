951827c523bd50fc95128126486a7a38c37f45e0
Avb2 <bringuel.alexander@gmail.com>
2025-06-16 23:23:38 -0400

    d

---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<script setup>
+import { RouterView } from 'vue-router';
+import Navbar from './components/Navbar.vue';
+</script>
+
+
+
+
+
+<template>
+  <Navbar/>
+  <RouterView/>
+</template>
+
+
+
+
+
+<style scoped>
+
+.topnav {
+  
+  position: fixed;
+  top: 0;
+  width: 100vw;
+  background-color: #333;
+  overflow: hidden;
+  z-index: 1000;
+}
+
+.topnav a {
+  float: left;
+  display: block;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 14px 20px;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+.topnav a:hover {
+  background-color: #575757;
+  color: white;
+}
+
+.topnav a.active {
+  background-color: #04AA6D;
+  color: white;
+}
+
+/* Add some margin to body so content isn't hidden under nav */
+body {
+  margin-top: 50px;
+}
+</style>
+
+
+
---
+++ src/components/Navbar.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="topnav">
+        <a class="active" href="#feed">Feed</a>
+        <a href="#post">New Post</a>
+        <a href="#profile">Profile</a>
+        <a href="#settings">Settings</a>
+    </div>
+</template>
+
+
+
+<style>
+</style>
